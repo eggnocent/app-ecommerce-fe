6767ebc3bc97712f37b8d98c682a9df79b6f7e60
eggnocent <egiwiratamaaa@gmail.com>
2025-05-16 15:15:04 +0700

    Feat - user profile integration

--- src/api/grpc/auth-interceptor.ts
+++ src/api/grpc/auth-interceptor.ts
@@ -5,13 +5,11 @@ import { RpcInterceptor } from '@protobuf-ts/runtime-rpc';
 export const authInterceptor: RpcInterceptor = {
     interceptUnary(next, method, input, options) {
         const accessToken = localStorage.getItem('access_token');
-
-        if (accessToken) {
             options.meta = {
                 ...options.meta,
-                authorization: `Bearer ${accessToken}`,
+                authorization: accessToken ?  `Bearer ${accessToken}` : '',
             }
-        }
+        
 
         return next(method, input, options);
     },
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,16 +1,73 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import PlainHeroSection from '../../components/PlainHeroSection/PlainHeroSection';
+import { getAuthClient } from '../../api/grpc/client';
+import Swal from 'sweetalert2';
+import { convertTimestampToDate } from '../../util/date';
+import { RpcError } from '@protobuf-ts/runtime-rpc';
+import { useAuthStore } from '../../store/auth';
 
 function Profile() {
     const location = useLocation();
     const navigate = useNavigate();
+    const logoutUser = useAuthStore(state => state.logout)
+    const [fullname, setFullname] = useState<string>();
+    const [email, setEmail] = useState<string>();
+
+
+    const [memberSince, setMemberSince] = useState<string>();
 
     useEffect(() => {
         if (location.pathname === '/profile') {
             navigate('/profile/change-password');
         }
     }, [navigate, location.pathname]);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            
+        try {
+            const resp = await getAuthClient().getProfile({})
+
+            if (resp.response.base?.isError ?? true) {
+                Swal.fire({
+                    title: 'terjadi kesalahan',
+                    text: 'silakan coba beberapa saat lagi',
+                    icon: 'error'
+                })
+                return
+            }
+
+            setFullname(resp.response.fullName);
+            setEmail(resp.response.email);
+
+            const date = convertTimestampToDate(resp.response.memberSince);
+            setMemberSince(date)
+        } catch (e) {
+            if (e instanceof RpcError) {
+                if (e.code == 'UNAUTHENTICATED') {
+                    logoutUser();
+                    localStorage.removeItem('access_token');
+                    Swal.fire({
+                        title: 'sesi telah berakhir',
+                        text: 'silakan login kembali',
+                        icon:'warning'
+                    })
+                    navigate('/');
+                    return
+                }
+            }
+
+            Swal.fire({
+                title: 'terjadi kesalahan',
+                text: 'silakan coba beberapa saat lagi',
+                icon:'warning'
+            })
+        }
+        }
+        
+        fetchProfile();
+    }, []);
 
     return (
         <>
@@ -25,19 +82,19 @@ function Profile() {
                                     <div className="col-md-4">
                                         <div className="form-group">
                                             <label className="text-black">Nama Lengkap</label>
-                                            <div className="form-control-plaintext">John Doe</div>
+                                            <div className="form-control-plaintext">{fullname}</div>
                                         </div>
                                     </div>
                                     <div className="col-md-4">
                                         <div className="form-group">
                                             <label className="text-black">Alamat Email</label>
-                                            <div className="form-control-plaintext">john.doe@example.com</div>
+                                            <div className="form-control-plaintext">{email}</div>
                                         </div>
                                     </div>
                                     <div className="col-md-4">
                                         <div className="form-group">
                                             <label className="text-black">Anggota Sejak</label>
-                                            <div className="form-control-plaintext">15 Januari 2024</div>
+                                            <div className="form-control-plaintext">{memberSince}</div>
                                         </div>
                                     </div>
                                 </div>

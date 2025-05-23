27b3a1199664fbd56787676d9c3d9d93674c4b25
eggnocent <egiwiratamaaa@gmail.com>
2025-05-23 10:32:17 +0700

    Feat - setup form for admin product

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,29 +1,83 @@
+import { useForm } from "react-hook-form";
+import FormInput from "../FormInput/FormInput";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
+const createProductSchema = yup.object().shape({
+    name: yup.string().required("nama produk wajib diisi"),
+    // price: yup.number().required("harga produk wajib diisi").typeError("harga produk tidak valid").moreThan(0, "harga produk harus lebih dari 0"),
+    description: yup.string(),
+    image: yup.mixed<FileList>().required("gambar produk wajib diisi")
+    .test("fileLength", "gambar produk wajib diisi", (FileList) => {
+        return FileList.length > 0
+    })
+    .test("fileType", "format gambar tidak sesuai/valid", (FileList) => {
+        return FileList && FileList.length > 0 ? ["image/jpeg", "image/png"].includes(FileList[0].type) : true
+    })
+})
+
+
+interface ProductFormValues{
+    name: string;
+    // price: number;
+    description?: string;
+    image: FileList;
+}
+
 function ProductForm() {
+    const form = useForm<ProductFormValues>({
+        resolver: yupResolver(createProductSchema),
+    });
+    const submitHandler = (values: ProductFormValues) => {
+        console.log(values)
+    }
+    
     return (
         <div className="p-4 p-lg-5 border bg-white">
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_name">Nama Produk <span className="text-danger">*</span></label>
-                <input type="text" className="form-control" id="product_name" placeholder="Nama Produk" />
-            </div>
+
+            <form onSubmit={form.handleSubmit(submitHandler)}>
+                <FormInput  <ProductFormValues>
+                    errors={form.formState.errors}
+                    name="name"
+                    register={form.register}
+                    type="text"
+                    label="Nama produk"
+                    placeholder="nama produk"
+                    labelRequired
+                
+                />
 
             <div className="form-group mb-3">
                 <label className="text-black" htmlFor="product_price">Harga <span className="text-danger">*</span></label>
                 <input type="number" className="form-control" id="product_price" placeholder="Harga Produk" />
             </div>
 
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_image">Gambar Produk <span className="text-danger">*</span></label>
-                <input type="file" className="form-control" id="product_image" accept="image/*" />
-            </div>
+            <FormInput<ProductFormValues>
+                    errors={form.formState.errors}
+                    name="image"
+                    register={form.register}
+                    type="image"
+                    label="gambar product"
+                    placeholder="gambar product"
+                    labelRequired
+                />
 
-            <div className="form-group mb-4">
-                <label className="text-black" htmlFor="product_description">Deskripsi</label>
-                <textarea className="form-control" id="product_description" rows={4} placeholder="Deskripsi produk..."></textarea>
-            </div>
+            <FormInput<ProductFormValues>
+                    errors={form.formState.errors}
+                    name="description"
+                    register={form.register}
+                    type="textarea"
+                    label="deskripsi"
+                    placeholder="deskripsi product..."
+                />
 
             <div className="form-group">
-                <button className="btn btn-primary">Simpan Produk</button>
+                <button className="btn btn-primary" type="submit">Simpan Produk</button>
             </div>
+
+            </form>
+            
         </div>
     )
 }
